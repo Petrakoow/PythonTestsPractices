15b0dfb9034140c77c80d5e789ce34f7acbcd2b2
Petrakowww <Petrakov.Egor.G@yandex.ru>
2024-09-26 16:17:27 +0300

    gnome sort

---
+++ FirstPractice/gnome_sort.py
@@ -0,0 +1,15 @@
+def gnome_sort(array):
+    if not isinstance(array, list):  # Проверка, что аргумент является списком!
+        raise TypeError("Аргумент должен быть списком")
+    
+    if not all(isinstance(x, (int, float)) for x in array):  # Проверка, что все элементы списка числа!
+        raise ValueError("Все элементы списка должны быть числами")
+
+    index = 0
+    while index < len(array):
+        if index == 0 or array[index] >= array[index - 1]:
+            index += 1
+        else:
+            array[index], array[index - 1] = array[index - 1], array[index]
+            index -= 1
+    return array
